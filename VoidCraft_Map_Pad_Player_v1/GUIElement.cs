3e1246c82080e747b352e2aaa0d8906a359938a5
DarkKonrad <makabrak@gmail.com>
2017-05-26 13:33:41 +0200

    Dodano Menu.  Narazie brak przycisku powrotu. Rozpoznaje przejścia np do opcji, gry. Po wciśnięciu Graj uruchamia się..gra

---
+++ VoidCraft_Map_Pad_Player_v1/GUIElement.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Input;
+namespace VoidCraft_Map_Pad_Player_v1
+{
+  public  class GUIElement
+    {
+        private Texture2D GUITexture;
+        private Rectangle GUIRect;
+        private string assetName;
+ 
+        
+
+        public string AssetName { get => assetName; set => assetName = value; }
+
+        public delegate void ElementClicked(string element);
+        public event ElementClicked clickEvent;
+
+        public GUIElement(string assetName)
+        {
+            this.assetName = assetName;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            GUITexture = content.Load<Texture2D>(assetName); // GUITEXTURE instancja Texture2D
+           
+    
+            
+            GUIRect = new Rectangle(0, 0, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width/17, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height/17);
+
+
+        }
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(GUITexture, GUIRect, Color.White);
+        }
+        public void Update()
+        {
+            TouchCollection touchcollection =  TouchPanel.GetState();
+          
+            
+            foreach (TouchLocation tl in touchcollection) // włazi w Kwadrat
+            if (GUIRect.Contains(tl.Position))
+            {
+                    clickEvent(assetName);
+            }
+      
+        }
+
+        public void CenterElement(int height,int width)
+        {
+                GUIRect = new Rectangle((width / 2) - (this.GUITexture.Width / 2), (height / 2) - (this.GUITexture.Height / 2), this.GUITexture.Width / 2, this.GUITexture.Height/2 );
+
+        }
+
+        public void MoveElement(int x,int y)
+        {
+            GUIRect = new Rectangle(GUIRect.X += x, GUIRect.Y += y, GUIRect.Width, GUIRect.Height);
+        }
+
+        public void Background()
+        {
+            GUIRect = new Rectangle(0, 0, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
+        }
+
+    }
+
+
+}
---
+++ VoidCraft_Map_Pad_Player_v1/MainMenu.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Android.Views.InputMethods;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
+using VoidCraft_Map_Pad_Player_v1;
+
+namespace VoidCraft_Map_Pad_Player_v1
+{
+ 
+
+
+    public class MainMenu
+    {
+
+        private int c = 1;
+        private Song mor;
+    
+        enum GameState { MainMenu,authors,inGame,Options}
+        GameState gamestate;
+
+        List<GUIElement> options = new List<GUIElement>();
+        List<GUIElement> main = new List<GUIElement>();
+      public  List<GUIElement> enterName = new List<GUIElement>();
+
+      //  private Keys
+        public MainMenu()
+        {
+           
+            main.Add(new GUIElement("Menu\\M_BACK"));
+            main.Add(new GUIElement("Menu\\B_graj"));
+            main.Add(new GUIElement("Menu\\B_ustawianie"));
+            main.Add(new GUIElement("Menu\\B_autorzy"));
+            main.Add(new GUIElement("Menu\\voidscraft"));
+            //  options.Add(new GUIElement("2"));
+            options.Add(new GUIElement("Menu\\Bot"));
+
+            enterName.Add(new GUIElement("Menu\\name"));
+            enterName.Add(new GUIElement("Menu\\done"));
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+           
+            foreach (GUIElement element in main)
+            {
+                element.LoadContent(content);
+
+                element.CenterElement((GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height-500)+250*c, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width);
+                c++;
+                element.clickEvent += OnClick;
+            }
+            c = 1;
+            main.Find(x => x.AssetName == "Menu\\M_BACK").Background();
+           main.Find(x => x.AssetName == "Menu\\voidscraft").MoveElement(0, -600);
+          
+            foreach (GUIElement element in enterName)
+            {
+                element.LoadContent(content);
+              
+                element.CenterElement((GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height-500) + 250 * c, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width);
+                element.clickEvent += OnClick;
+                c++;
+            }
+
+
+            foreach(GUIElement element in options)
+            {
+             
+                element.LoadContent(content);
+                element.CenterElement(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width);
+                element.clickEvent += OnClick;
+            }
+
+        }
+
+        public void Update()
+        {
+            switch (gamestate)
+            {
+                case GameState.MainMenu:
+                    foreach (GUIElement element in main)
+                    {
+                        element.Update();
+                    }
+                    break;
+                case GameState.authors:
+                    foreach (GUIElement element in enterName)
+                    {
+                        element.Update();
+                    }
+                    break;
+                case GameState.inGame:
+                    break;
+                case GameState.Options:
+                    foreach(GUIElement element in options)
+                    {
+                        element.Update();
+                    }
+
+                    break;
+            }
+          
+        
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            switch (gamestate)
+            {
+                case GameState.MainMenu:
+                    foreach (GUIElement element in main)
+                    {
+                        element.Draw(spriteBatch);
+
+                    }
+                    break;
+                case GameState.authors:
+                    foreach (GUIElement element in enterName)
+                    {
+                        
+                        element.Draw(spriteBatch);
+                       
+                    }
+                    break;
+                case GameState.inGame:
+                    break;
+                case GameState.Options:
+                    foreach(GUIElement element in options)
+                    {
+                        element.Draw(spriteBatch);
+                    }
+
+                    break;
+
+            }
+           
+          
+        }
+
+     
+        public void OnClick(string element)
+        {
+            if (element=="Menu\\B_graj")
+            {
+                Game1.Running = true;
+                // plays the game
+                gamestate = GameState.inGame;
+            }
+            if(element== "Menu\\B_autorzy")
+            {
+               gamestate = GameState.authors;
+               
+
+            }
+            if (element == "Menu\\done")//Done button
+            {
+                gamestate = GameState.MainMenu;
+            }
+            if (element== "Menu\\B_ustawianie")
+            {
+                gamestate = GameState.Options;
+            }
+            if (element== "Menu\\Bot")
+            {
+            // To do: Tuaj będą "ustawienia" jakieś pomysły ?
+            }
+        }
+        //private void GetKeys()
+        //{
+        //    KeyboardState state = Keyboard.GetState();
+        //   // TouchCollection state = TouchPanel.GetState();
+        //    System.Text.StringBuilder sb = new StringBuilder();
+        //    foreach (var key in state.GetPressedKeys())
+        //        sb.Append(key);
+
+        //    if (sb.Length > 0) System.Diagnostics.Debug.WriteLine(sb.ToString());
+        //    else
+        //        System.Diagnostics.Debug.WriteLine("NIc sie nie wcisnęło");
+
+        //}
+    }
+}
